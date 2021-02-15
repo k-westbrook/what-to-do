4bee894d1e6f3d9a117df0c3b48b77bf6182a6b5
k-westbrook <kcheriewestbrook@gmail.com>
2021-02-15 00:24:30 -0800

    add enum

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,24 @@
-import { Button, TextField, Typography } from '@material-ui/core';
+import {
+  Button,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from '@material-ui/core';
 import React, { FunctionComponent } from 'react';
 import { FormItem } from './structure/FormItem';
 import { FormWrapper } from './structure/FormWrapper';
 import { TextWrapper } from './structure/TextWrapper';
 
+enum Category {
+  SOCIAL,
+  WORK,
+  HOME,
+  ERRAND,
+  TRAVEL,
+  PET,
+  OTHER,
+}
 const Form: FunctionComponent = () => {
   return (
     <FormWrapper>
@@ -14,6 +29,17 @@ const Form: FunctionComponent = () => {
         <Button variant="contained" style={{ height: '100%' }}>
           Add a Task
         </Button>
+      </FormItem>
+      <FormItem>
+        <Select value={Category.SOCIAL} style={{ width: '200px' }}>
+          <MenuItem value={Category.SOCIAL}>Social</MenuItem>
+          <MenuItem value={Category.WORK}>Work</MenuItem>
+          <MenuItem value={Category.HOME}>Home</MenuItem>
+          <MenuItem value={Category.ERRAND}>Errand</MenuItem>
+          <MenuItem value={Category.TRAVEL}>Travel</MenuItem>
+          <MenuItem value={Category.PET}>Pet</MenuItem>
+          <MenuItem value={Category.OTHER}>Other</MenuItem>
+        </Select>
       </FormItem>
     </FormWrapper>
   );
