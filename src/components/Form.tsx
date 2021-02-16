5dd629a2034e0c900640cb995f0023683f19f86d
k-westbrook <kcheriewestbrook@gmail.com>
2021-02-15 22:39:19 -0800

    test form with react hooks

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,10 +5,17 @@ import {
   TextField,
   Typography,
 } from '@material-ui/core';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { FormItem } from './structure/FormItem';
 import { FormWrapper } from './structure/FormWrapper';
 
+interface todo {
+  category: Category;
+  name: string;
+  date: any;
+  time: any;
+  complete: boolean;
+}
 enum Category {
   SOCIAL,
   WORK,
@@ -18,34 +25,71 @@ enum Category {
   PET,
   OTHER,
 }
+
+const defaultTodo = {
+  category: Category.OTHER,
+  name: null,
+  date: null,
+  time: null,
+  complete: false,
+};
+
 const Form: FunctionComponent = () => {
+  const [todo, setTodo] = useState(defaultTodo);
+  const handleChange = (name: any) => (event: any) => {
+    setTodo({ ...todo, [name]: event.target.value });
+  };
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+  };
   return (
     <FormWrapper>
-      <FormItem flex>
-        <Select value={Category.SOCIAL} style={{ width: '200px' }}>
-          <MenuItem value={Category.SOCIAL}>Social</MenuItem>
-          <MenuItem value={Category.WORK}>Work</MenuItem>
-          <MenuItem value={Category.HOME}>Home</MenuItem>
-          <MenuItem value={Category.ERRAND}>Errand</MenuItem>
-          <MenuItem value={Category.TRAVEL}>Travel</MenuItem>
-          <MenuItem value={Category.PET}>Pet</MenuItem>
-          <MenuItem value={Category.OTHER}>Other</MenuItem>
-        </Select>
-      </FormItem>
-      <FormItem>
-        <TextField variant="outlined" />
-      </FormItem>
-      <FormItem flex alignItems="flex-end">
-        <TextField type="date" defaultValue="2017-05-24" />
-      </FormItem>
-      <FormItem flex alignItems="flex-end">
-        <TextField type="time" defaultValue="07:30" />
-      </FormItem>
-      <FormItem>
-        <Button variant="contained" style={{ height: '100%' }}>
-          Add a Task
-        </Button>
-      </FormItem>
+      <form onSubmit={handleSubmit}>
+        <FormItem flex>
+          <Select
+            name="category"
+            value={todo.category}
+            style={{ width: '200px' }}
+            onChange={handleChange('category')}
+          >
+            <MenuItem value={Category.SOCIAL}>Social</MenuItem>
+            <MenuItem value={Category.WORK}>Work</MenuItem>
+            <MenuItem value={Category.HOME}>Home</MenuItem>
+            <MenuItem value={Category.ERRAND}>Errand</MenuItem>
+            <MenuItem value={Category.TRAVEL}>Travel</MenuItem>
+            <MenuItem value={Category.PET}>Pet</MenuItem>
+            <MenuItem value={Category.OTHER}>Other</MenuItem>
+          </Select>
+        </FormItem>
+        <FormItem>
+          <TextField
+            name="todo"
+            variant="outlined"
+            onChange={handleChange('name')}
+          />
+        </FormItem>
+        <FormItem flex alignItems="flex-end">
+          <TextField
+            name="date"
+            type="date"
+            defaultValue="2017-05-24"
+            onChange={handleChange('date')}
+          />
+        </FormItem>
+        <FormItem flex alignItems="flex-end">
+          <TextField
+            name="time"
+            type="time"
+            defaultValue="07:30"
+            onChange={handleChange('time')}
+          />
+        </FormItem>
+        <FormItem>
+          <Button type="submit" variant="contained" style={{ height: '100%' }}>
+            Add a Task
+          </Button>
+        </FormItem>
+      </form>
     </FormWrapper>
   );
 };
